6c2610742e18b4513d588834c5883c62de29a7e5
Oren Leavitt <oleavitt@ix.netcom.com>
2025-01-26 23:56:07 -0600

    Initial skeleton project commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Oren Leavitt on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
